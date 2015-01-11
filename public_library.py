7905b96d4844d10d408950820ec402a5bf8afef9
Mark Saiget <bm5w@users.noreply.github.com>
2015-01-10 17:06:07 -0800

    change shelf naming and report methods

--- public_library.py
+++ public_library.py
@@ -18,51 +18,56 @@ class Library(object):
 
     def report(self):
         """Report all books Library contains."""
-        print 'This Library contains the following books:\nAuthor\t\tTitle\t\t\
-        ISBN\t\tShelf'
+        print 'This Library contains the following books (AUTHOR, \"TITLE\", ISBN):'
         for shelf in self.shelves:
-            for book in shelf:
-                print book.output() + '\t\t{shelf}'.format(shelf=shelf)
+            shelf.output()
 
 
 class Shelf(object):
     """The Shelf class is made up of books."""
-    def __init__(self, name, library):
-        library.shelves.append(name)
+    def __init__(self, library, name):
+        library.shelves.append(self)
+        self.name = name
         self.books = []
 
+    def output(self):
+        """Print list of all books on shelf."""
+        print '{shelf} shelf contents:'.format(shelf=self.name)
+        for book in self.books:
+            print book.output()
 
 class Book(object):
     """A class defining each book in the library"""
 
     def __init__(self, author, title, ISBN):
         """Initialize a book, with Author Name, Title and ISBN"""
-        self.data = {author, title, ISBN}      # A set to define the book
+        self.data = [author, title, ISBN]      # A list to define the book
 
     def enshelf(self, shelf):
         """Add book to a particular shelf."""
-        shelf.books.append(self.Book)
+        shelf.books.append(self)
 
     def unshelf(self, shelf):
         """Remove book from shelf."""
-        shelf.books.pop(self.Book)
+        shelf.books.pop(self)
 
     def output(self):
         """Return string representation of book"""
-        return '{author},       {title},        {ISBN}\n'.\
+        return '{author}, \"{title}\", {ISBN}'.\
             format(author=self.data[0], title=self.data[1], ISBN=self.data[2])
 
 if __name__ == '__main__':
     """Test classes on execution."""
     a = Library()   # Create library
-    shelf1 = Shelf('1', a)  # Add shelves to library
-    shelf2 = Shelf('2', a)
+    shelf1 = Shelf(a, 'Fiction')  # Add shelves to library
+    shelf2 = Shelf(a, 'Non-Fiction')
     author_list = ['Mark Saiget', 'Joe Smith', 'Michael Creighton',
-                   'Salman Rushdie', 'William Faulkner']
+                   'Salman Rushdie', 'William Faulkner', 'Joel Schmit']
     title_list = ['Python', 'The Life of Nexus', 'Jurassic Park',
-                  'Midnight\'s Children', 'As I Lay Dying']
+                  'Midnight\'s Children', 'As I Lay Dying', 'Adrift']
     isbn_list = [1234567890, 1234567899, 1234567888,
                  1234567777, 1234566666, 1234555555]
     shelf_list = [shelf1, shelf2, shelf1, shelf2, shelf1, shelf2]
     for author, title, isbn, shelf in zip(author_list, title_list, isbn_list, shelf_list):
-        Book(author, title, isbn, shelf)
+        Book(author, title, isbn).enshelf(shelf)
+    a.report()
